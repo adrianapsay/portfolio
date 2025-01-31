125e93dc3312e4df8464f5d632ee1e996301e73c
Adrian <110417415+adrianapsay@users.noreply.github.com>
2025-01-31 15:07:13 -0800

    additional changes lab4

--- projects/projects.js
+++ projects/projects.js
@@ -3,5 +3,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 (async function() {
     const projects = await fetchJSON('../lib/projects.json');
     const projectsContainer = document.querySelector('.projects');
+    const projectsTitle = document.querySelector('.projects-title');
+    projectsTitle.textContent = `${projects.length} Projects`;
     renderProjects(projects, projectsContainer, 'h2');
 })();
