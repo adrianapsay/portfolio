a87e042c08b192c258029d67fdec254fc8516436
Adrian <110417415+adrianapsay@users.noreply.github.com>
2025-02-08 21:40:04 -0800

    some of the lab finished

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 (async function() {
     const projects = await fetchJSON('../lib/projects.json');
@@ -7,3 +8,72 @@ import { fetchJSON, renderProjects } from '../global.js';
     projectsTitle.textContent = `${projects.length} Projects`;
     renderProjects(projects, projectsContainer, 'h2');
 })();
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+// let arc = arcGenerator({
+//     startAngle: 0,
+//     endAngle: 2 * Math.PI,
+//   });
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red')
+
+
+// let data = [1, 2, 3, 4, 5, 5];
+// let total = 0;
+
+// for (let d of data) {
+//   total += d;
+// }
+
+// let angle = 0;
+// let arcData = [];
+
+// for (let d of data) {
+//   let endAngle = angle + (d / total) * 2 * Math.PI;
+//   arcData.push({ startAngle: angle, endAngle });
+//   angle = endAngle;
+// }
+
+let projects = await fetchJSON('../lib/projects.json'); // fetch your project data
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+  });
+
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(idx))
+})
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`)
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+});
+
+
+let query = '';
+
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('change', (event) => {
+  // Update query value
+  query = event.target.value;
+  // Filter projects based on all field values
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+});
