6be3f2934b7d9cd701a8180e98bb327a119c61f4
Adrian <110417415+adrianapsay@users.noreply.github.com>
2025-02-24 18:58:30 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,7 @@
 import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
+// global vars
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 console.log(projectsContainer)
